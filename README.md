2ac1ff668b08919b06da624984540c874d055d3f
Guilherme Gomes <guilhermeg.contato@gmail.com>
2024-03-16 18:13:34 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # wikiForFansVikingsService
 This repository for backend of the application Wiki For Fans - Vikings
+
+
+
+ This is the component responsible for providing the logic and API endpoints for the Vikings Series Fan Wiki application.
+
+## Backend (Node with Express and Nodemon)
+
+## How to Start the Backend in a Local Environment:
+
+1. **Requirements
+   - Node.js installed (version 14 or higher)
+   - NPM or Yarn
+
+2. **Clone the Repository (if you haven't already done so):**
+   ```bash
+   git clone https://github.com/seu-usuario/wiki-fas-vikings.git
+
+3. **Execute:**
+    ```bash	
+    npm install or yarn install
+
+4. **Start**
+    ```bash	
+    nodemon start
---
+++ src/api/data/characters-vikings.js
@@ -0,0 +1,185 @@
+const charactersVikings =  
+[
+    {
+        "id": 1,
+        "characterName": "Ragnar Lothbrok",
+        "actorName": "Travis Fimmel",
+        "episodeCount": 45,
+        "roleDescription": "A farmer and warrior who becomes a legendary Norse leader.",
+        "series": "Vikings",
+        "img": "../src//assets/img/characters/vikings/ragnar.jpg"
+    },
+    {
+        "id": 2,
+        "characterName": "Lagertha",
+        "actorName": "Katheryn Winnick",
+        "episodeCount": 44,
+        "roleDescription": "A warrior and Ragnar's wife, known for her strength and combat abilities.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/lagertha.jpg"
+    },
+    {
+        "id": 3,
+        "characterName": "Floki",
+        "actorName": "Gustaf Skarsgård",
+        "episodeCount": 45,
+        "roleDescription": "A boat builder and loyal follower of Ragnar, known for his devotion to the Norse gods.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/floki.jpg"
+    },
+    {
+        "id": 4,
+        "characterName": "Rollo",
+        "actorName": "Clive Standen",
+        "episodeCount": 45,
+        "roleDescription": "A warrior and Ragnar's brother who becomes an important leader in France.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/rollo.jpg"
+    },
+    {
+        "id": 5,
+        "characterName": "Bjorn Ironside",
+        "actorName": "Alexander Ludwig",
+        "episodeCount": 45,
+        "roleDescription": "Son of Ragnar and Lagertha, known for his leadership and exploration skills.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/bjorn.jpg"
+    },
+    {
+        "id": 6,
+        "characterName": "Ragnar Lothbrok",
+        "actorName": "Travis Fimmel",
+        "episodeCount": 45,
+        "roleDescription": "A farmer and warrior who becomes a legendary Norse leader.",
+        "series": "Vikings",
+        "img": "../src//assets/img/characters/vikings/ragnar.jpg"
+    },
+    {
+        "id": 7,
+        "characterName": "Lagertha",
+        "actorName": "Katheryn Winnick",
+        "episodeCount": 44,
+        "roleDescription": "A warrior and Ragnar's wife, known for her strength and combat abilities.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/lagertha.jpg"
+    },
+    {
+        "id": 8,
+        "characterName": "Floki",
+        "actorName": "Gustaf Skarsgård",
+        "episodeCount": 45,
+        "roleDescription": "A boat builder and loyal follower of Ragnar, known for his devotion to the Norse gods.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/floki.jpg"
+    },
+    {
+        "id": 9,
+        "characterName": "Rollo",
+        "actorName": "Clive Standen",
+        "episodeCount": 45,
+        "roleDescription": "A warrior and Ragnar's brother who becomes an important leader in France.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/rollo.jpg"
+    },
+    {
+        "id": 10,
+        "characterName": "Bjorn Ironside",
+        "actorName": "Alexander Ludwig",
+        "episodeCount": 45,
+        "roleDescription": "Son of Ragnar and Lagertha, known for his leadership and exploration skills.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/bjorn.jpg"
+    },
+    {
+        "id": 11,
+        "characterName": "Ragnar Lothbrok",
+        "actorName": "Travis Fimmel",
+        "episodeCount": 45,
+        "roleDescription": "A farmer and warrior who becomes a legendary Norse leader.",
+        "series": "Vikings",
+        "img": "../src//assets/img/characters/vikings/ragnar.jpg"
+    },
+    {
+        "id": 12,
+        "characterName": "Lagertha",
+        "actorName": "Katheryn Winnick",
+        "episodeCount": 44,
+        "roleDescription": "A warrior and Ragnar's wife, known for her strength and combat abilities.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/lagertha.jpg"
+    },
+    {
+        "id": 13,
+        "characterName": "Floki",
+        "actorName": "Gustaf Skarsgård",
+        "episodeCount": 45,
+        "roleDescription": "A boat builder and loyal follower of Ragnar, known for his devotion to the Norse gods.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/floki.jpg"
+    },
+    {
+        "id": 14,
+        "characterName": "Rollo",
+        "actorName": "Clive Standen",
+        "episodeCount": 45,
+        "roleDescription": "A warrior and Ragnar's brother who becomes an important leader in France.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/rollo.jpg"
+    },
+    {
+        "id": 15,
+        "characterName": "Bjorn Ironside",
+        "actorName": "Alexander Ludwig",
+        "episodeCount": 45,
+        "roleDescription": "Son of Ragnar and Lagertha, known for his leadership and exploration skills.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/bjorn.jpg"
+    },
+    {
+        "id": 16,
+        "characterName": "Ragnar Lothbrok",
+        "actorName": "Travis Fimmel",
+        "episodeCount": 45,
+        "roleDescription": "A farmer and warrior who becomes a legendary Norse leader.",
+        "series": "Vikings",
+        "img": "../src//assets/img/characters/vikings/ragnar.jpg"
+    },
+    {
+        "id": 17,
+        "characterName": "Lagertha",
+        "actorName": "Katheryn Winnick",
+        "episodeCount": 44,
+        "roleDescription": "A warrior and Ragnar's wife, known for her strength and combat abilities.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/lagertha.jpg"
+    },
+    {
+        "id": 18,
+        "characterName": "Floki",
+        "actorName": "Gustaf Skarsgård",
+        "episodeCount": 45,
+        "roleDescription": "A boat builder and loyal follower of Ragnar, known for his devotion to the Norse gods.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/floki.jpg"
+    },
+    {
+        "id": 19,
+        "characterName": "Rollo",
+        "actorName": "Clive Standen",
+        "episodeCount": 45,
+        "roleDescription": "A warrior and Ragnar's brother who becomes an important leader in France.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/rollo.jpg"
+    },
+    {
+        "id": 20,
+        "characterName": "Bjorn Ironside",
+        "actorName": "Alexander Ludwig",
+        "episodeCount": 45,
+        "roleDescription": "Son of Ragnar and Lagertha, known for his leadership and exploration skills.",
+        "series": "Vikings",
+        "img": "../src/assets/img/characters/vikings/bjorn.jpg"
+    }
+];
+
+module.exports = charactersVikings;
